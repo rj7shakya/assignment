0906a21721f1962e1fab3205e472a7c3189d9db8
rj7shakya <shakyarajad1@gmail.com>
2023-06-13 14:00:02 +0545

    assignment code

---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+import "./button.css";
+
+const Button = () => {
+  return (
+    <button className="button">
+      <img src="pp.png" alt="cant load" />
+      Click Me!
+    </button>
+  );
+};
+
+export default Button;
